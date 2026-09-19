7c81b30c50c9b7fb9cb9e5ec70c1f317a938d14c
Devananda2006 <devanandajoshy97@gmail.com>
2026-09-19 21:32:17 +0530

    Day 3: Added JavaScript DOM interactions

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+// ===============================
+// Explore Button
+// ===============================
+
 const button = document.getElementById("exploreBtn");
 
 button.addEventListener("click", function () {
-    alert("Welcome to Smart Campus Hub!");
+
+    alert("Here are the latest events happening on your campus!");
+
+    description.classList.toggle("highlight");
+
 });
 
 
+// ===============================
+// Registration Form
+// ===============================
+
 const form = document.querySelector("form");
+
+const registrationMessage =
+    document.getElementById("registrationMessage");
+
 
 form.addEventListener("submit", function (event) {
 
@@ -27,18 +43,80 @@ form.addEventListener("submit", function (event) {
         reason === "" ||
         eventName === ""
     ) {
+
         alert("Please fill in all the fields.");
+
         return;
     }
 
 
-    alert(`Registration successful!
-
-Welcome, ${name}.
-
-You registered for ${eventName}.`);
+    registrationMessage.textContent =
+        `Registration successful! Welcome, ${name}. You registered for ${eventName}.`;
 
 
     form.reset();
 
 });
+
+
+// ===============================
+// Title
+// ===============================
+
+const title = document.querySelector("h1");
+
+title.addEventListener("click", function () {
+
+    alert("You clicked the title!");
+
+});
+
+title.textContent = "Smart Campus OS";
+
+
+// ===============================
+// Description
+// ===============================
+
+const description = document.querySelector("main p");
+
+description.textContent =
+    "Your campus. Your events. Your community.";
+
+
+// ===============================
+// Event Buttons
+// ===============================
+
+const eventMessage =
+    document.getElementById("eventMessage");
+
+const eventButtons =
+    document.querySelectorAll(".event button");
+
+
+eventButtons.forEach(button => {
+
+    button.addEventListener("click", function () {
+
+        const event = button.parentElement;
+
+        const eventName = event.querySelector("h3");
+
+        const details = event.querySelectorAll("p");
+
+        details[1].classList.toggle("highlight");
+
+        eventMessage.textContent =
+            "You selected " + eventName.textContent;
+
+    });
+
+});
+
+
+// ===============================
+// Check number of event buttons
+// ===============================
+
+console.log(eventButtons.length);
